b4e686ff0719afea1b8258d0cc242bfc799e132f
Junkyu Park <joon3216@gmail.com>
2019-06-06 16:15:17 -0400

    Second update to .Rd files in man folder

--- R/pgf.R
+++ R/pgf.R
@@ -1,13 +1,6 @@
 
 dpmf <- function(x, pmf_vec, support_vec) {
-    # x: a number, or a vector whose class is the same as support_vec.
-    # pmf_vec: a numeric vector, where all elements fall into (0, 1) and 
-    #          sum up to 1.
-    # support_vec: a vector with the same length as pmf_vec where
-    #              each entry of support_vec is the input that corresponds to
-    #              the probability in pmf_vec; if missing, it is replaced 
-    #              with 0:(length(pmf_vec) - 1).
-    
+
     M <- length(pmf_vec)
     if (missing(support_vec)) {
         names(pmf_vec) <- 0:(M - 1)
@@ -27,10 +20,7 @@ dpmf <- function(x, pmf_vec, support_vec) {
 }
 
 rpmf <- function(n, pmf, support, ...) {
-    # n: an integer
-    # pmf, support: the same as pmf_vec and support_vec in dpmf respectively
-    # ...: additional arguments of pmf
-    
+
     cdf <- c(0, cumsum(pmf(support, ...)))
     unif_01 <- runif(n)
     result <- numeric(length = n)
